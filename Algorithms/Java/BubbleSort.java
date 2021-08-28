b232ad419703721f13a7487c24f5a633726c13d5
Sashi445 <sashidhar652@gmail.com>
2021-08-28 22:35:39 +0530

    Added bubble sort implementation in java!

---
+++ Algorithms/Java/BubbleSort.java
@@ -0,0 +1,27 @@
+public class BubbleSort {
+
+    public static void swap(int[] arr, int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static void sort(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j < arr.length - 1; j++){
+                if(arr[j] > arr[j+1]){
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {8, 3, 2, 9,  1};
+        BubbleSort.sort((arr));
+        for(int num : arr){
+            System.out.print(num);
+        }
+    }
+    
+}
